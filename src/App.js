5b466c803c4faff6cfc546d63d2a6c822230adfd
storm <stooormix@gmail.com>
2022-02-15 00:43:48 +0330

    finish create card

--- src/App.js
+++ src/App.js
@@ -9,35 +9,35 @@ export default function App() {
         <div className='card-expertise'>
           <div className='card-expertise-html'>
             <span>HTML</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/html.svg' />
           </div>
           <div className='card-expertise-css'>
             <span>CSS</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/css.svg' />
           </div>
           <div className='card-expertise-javascript'>
             <span>JAVASCTIPT</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/javascript.svg' />
           </div>
           <div className='card-expertise-bootstrap'>
             <span>BOOTSTRAP</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/bootstrap.svg' />
           </div>
-          <div className='card-expertise-jquary'>
-            <span>JQUARY</span>
-            <img src='' />
+          <div className='card-expertise-jquery'>
+            <span>JQUERY</span>
+            <img src='./IMG/logo_lessen/jquery.svg' />
           </div>
           <div className='card-expertise-sass'>
             <span>SASS</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/sass.svg' />
           </div>
           <div className='card-expertise-react'>
             <span>REACT</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/react.svg' />
           </div>
           <div className='card-expertise-git'>
             <span>GIT</span>
-            <img src='' />
+            <img src='./IMG/logo_lessen/git.svg' />
           </div>
         </div>
       </section>
