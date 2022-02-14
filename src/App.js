135d3c97357813c60cba8967312d02760d6ec427
storm <stooormix@gmail.com>
2022-02-14 23:46:00 +0330

    create section expertise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
 
 
 export default function App() {
   return (
-    <div>App</div>
+    <Fragment>
+      <section className='card'>
+        <div className='card-expertise'>
+          <div className='card-expertise-html'>
+            <span>HTML</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-css'>
+            <span>CSS</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-javascript'>
+            <span>JAVASCTIPT</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-bootstrap'>
+            <span>BOOTSTRAP</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-jquary'>
+            <span>JQUARY</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-sass'>
+            <span>SASS</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-react'>
+            <span>REACT</span>
+            <img src='' />
+          </div>
+          <div className='card-expertise-git'>
+            <span>GIT</span>
+            <img src='' />
+          </div>
+        </div>
+      </section>
+    </Fragment>
   )
 }
