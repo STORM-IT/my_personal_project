324d0157d1748a898e2f22235a4de1993caf00d4
storm <stooormix@gmail.com>
2022-02-15 23:18:19 +0330

    add picture and change size to low

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 
-import {url} from './component/utils/video_short_head'
+import { url } from './component/utils/video_short_head'
 
 
 export default function App() {
 
-  const [url,setUrl]=useState("./IMG/head_short_video/top-view-of-business-handshake-and-client-meeting.m4v");
-  const [currentVideo,setCurrentVideo] =useState(1);
-  
-  const videos=["./IMG/head_short_video/top-view-of-business-handshake-and-client-meeting.mp4","./IMG/head_short_video/computer-code-in-the-screen.mp4","./IMG/head_short_video/open-office-space.m4v"];
+  const [url, setUrl] = useState("./IMG/head_short_video/top-view-of-business-handshake-and-client-meeting.m4v");
+  const [currentVideo, setCurrentVideo] = useState(1);
+
+  const videos = ["./IMG/head_short_video/top-view-of-business-handshake-and-client-meeting.m4v", "./IMG/head_short_video/computer-code-in-the-screen.mp4", "./IMG/head_short_video/open-office-space.m4v"];
 
 
   const vidRef = useRef();
@@ -18,22 +18,23 @@ export default function App() {
     // setUrl(videos[1])
     vidRef.current.play();
   }, [])
-  
+
   useEffect(() => {
     console.log("effect")
     vidRef.current.play();
   }, [url])
-  
 
-  const next_video=()=>{
+
+  const next_video = () => {
     if (currentVideo === videos.length) {
+      // debugger
       setCurrentVideo(0);
-      setUrl(videos[currentVideo])
+      setUrl(videos[0])
     } else {
-      setCurrentVideo(currentVideo+1)
+      setCurrentVideo(currentVideo + 1)
       setUrl(videos[currentVideo])
-      }
     }
+  }
 
   return (
     <Fragment>
@@ -43,17 +44,15 @@ export default function App() {
         {/* <source src='./IMG/head_short_video/top-view-of-business-handshake-and-client-meeting.mp4' type="video/mp4"/> */}
         {/* </video> */}
         <div className='header-short'>
-          <video id='short-video_head' className='header-short-video' src={url} onEnded={()=>next_video()} ref={vidRef} muted />
-              <span>به وبساخت شخصی من خوش آمدید</span>
-              <svg className='header-short-footer' xmlns="http://www.w3.org/2000/svg" viewBox="0 -20 1440 200">
-                <path fill="#393e46" fill-opacity="1" d="M0,96L80,90.7C160,85,320,75,480,90.7C640,107,800,149,960,149.3C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-              </svg>
+          <video id='short-video_head' className='header-short-video' src={url} onEnded={() => next_video()} ref={vidRef} muted />
+          <span>به وبساخت شخصی من خوش آمدید</span>
+          <svg className='header-short-footer' xmlns="http://www.w3.org/2000/svg" viewBox="0 -20 1440 200">
+            <path fill="#393e46" fill-opacity="1" d="M0,96L80,90.7C160,85,320,75,480,90.7C640,107,800,149,960,149.3C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+          </svg>
+        </div>
 
 
-            </div>
-          
-
-            {/* <div>
+        {/* <div>
         <nav>
           <select>
             <option value={"home"}>خانه</option>
@@ -68,8 +67,23 @@ export default function App() {
             STORM
           </span>
         </div> */}
-          </header>
-          {/* <section className='card'>
+      </header>
+      <main>
+        <section className='main-information'>
+          <img className='main-information-picture' src='./IMG/information/final/IMG_20220210_180308.jpg' />
+          <div className='main-information-container'>
+            <h3 className='main-information-container-head'>اطلاعات کلی درباره</h3>
+            <p className='main-information-container-text'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+               و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با 
+              هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+               جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص
+               طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+               دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی
+               دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+          </div>
+        </section>
+      </main>
+      {/* <section className='card'>
             <div className='card-expertise'>
               <div className='card-expertise-html'>
                 <span>HTML</span>
@@ -105,6 +119,6 @@ export default function App() {
               </div>
             </div>
           </section> */}
-        </Fragment>
-        )
+    </Fragment>
+  )
 }
