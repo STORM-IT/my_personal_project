3c6743a520fc86cadd317258c1802ab11801183a
storm <stooormix@gmail.com>
2022-02-19 23:43:59 +0330

    set css div in socialNetwork

--- src/component/Layout/mainLayout/SocialNetwork.js
+++ src/component/Layout/mainLayout/SocialNetwork.js
@@ -5,8 +5,10 @@ export default function SocialNetwork() {
     <div className='SocialNetwork'>
         <div className='line'>
         <img id='659' src='./IMG/logo_lessen/github.svg'/>
+        <div>
 
-        <a href='https://github.com/STORM-IT' className='Username'>Account</a>
+        <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>Account</a>
+        </div>
         </div>
     </div>
   )
