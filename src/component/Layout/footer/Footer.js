602208cccdd3d831085514b2192fe57133e50cc8
storm <stooormix@gmail.com>
2022-02-21 00:41:12 +0330

    add animation from the main layout

--- src/component/Layout/footer/Footer.js
+++ src/component/Layout/footer/Footer.js
@@ -4,8 +4,13 @@ export default function Footer() {
   return (
     <div className='footer'>
       <div className='footer-head'>
-        <div className='footer-head-left'></div>
-        <div className='footer-head-right'></div>
+        <div className='footer-head-left'>
+          <p></p>
+          <p></p>
+        </div>
+        <div className='footer-head-right'>
+          
+        </div>
       </div>
       <div className='footer-creator'>این سایت متعلق به علی پارسامنش میباشد  </div>
     </div>
