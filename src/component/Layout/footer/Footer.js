ecd1402f8a207a4308fd823800edf1967ee02e7a
storm <stooormix@gmail.com>
2022-02-20 23:28:35 +0330

    create layout footer

---
+++ src/component/Layout/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-head'>
+        <div className='footer-head-left'></div>
+        <div className='footer-head-right'></div>
+      </div>
+      <div className='footer-creator'></div>
+    </div>
+  )
+}
