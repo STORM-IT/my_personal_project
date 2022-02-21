f0162e3b07da62fc30e4d3512b438051ca4adb2f
storm <stooormix@gmail.com>
2022-02-21 03:30:27 +0330

    create footer and set css/content (left section)

--- src/component/Layout/footer/Footer.js
+++ src/component/Layout/footer/Footer.js
@@ -5,11 +5,29 @@ export default function Footer() {
     <div className='footer'>
       <div className='footer-head'>
         <div className='footer-head-left'>
-          <p></p>
-          <p></p>
+          <div className='footer-head-left-telegram' >
+            <img  />
+            <a href='' className='telegram'>PSMNS@</a>
+          </div>
+          <div className='footer-head-left-instagram' >
+            <img />
+            <a href='' className='instagram'>ali._.psm</a>
+          </div>
+          <div className='footer-head-left-whatsApp' >
+            <img />
+            <a href='' className='whatsApp'>09370915132</a>
+          </div>
+          <div className='footer-head-left-call' >
+            <img />
+            <a href='' className='call'>09370915132</a>
+          </div>
+          <div className='footer-head-left-location' >
+            <img />
+            <a href='' className='location'>مشهد / سیمتر طلاب /رسیدی زنگ بزن میام بیرون</a>
+          </div>
         </div>
         <div className='footer-head-right'>
-          
+
         </div>
       </div>
       <div className='footer-creator'>این سایت متعلق به علی پارسامنش میباشد  </div>
