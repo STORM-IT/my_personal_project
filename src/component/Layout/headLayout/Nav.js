7342fbee6c5bbd792390a6f51d71c7529b68e9d6
storm <stooormix@gmail.com>
2022-02-21 19:11:05 +0330

    create option in nav with div and p / set css and position

--- src/component/Layout/headLayout/Nav.js
+++ src/component/Layout/headLayout/Nav.js
@@ -1,16 +1,27 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 export default function Nav() {
+    
+    
+
     return (
         <Fragment>
             <div id="head-nav" className='nav'>
                 <nav className='nav-container'>
-                    <a href='#head-nav'>خانه</a>
-                    <a href='#information'>اطلاعات کلی</a>
-                    <a href='#expertise'>تکنولوژی ها</a>
-                    <a href='#project'>پروژه ها</a>
-                    <a href='#SocialNetwork'>شبکه های اجتماعی</a>
+                    <div className='select'>
+                        <div className='select-input'>
+                            <p>بخش های سایت</p>
+                        </div>
+                        <div className='select-option'>
+                            <a href='#head-nav' >خانه</a>
+                            <a href='#information' >اطلاعات کلی</a>
+                            <a href='#expertise' >تکنولوژی ها</a>
+                            <a href='#project' >پروژه ها</a>
+                            <a href='#SocialNetwork'>شبکه های اجتماعی</a>
+                        </div>
+                    </div>
                 </nav>
+                  
             </div>
         </Fragment>
     )
