8f56361915934417b019fe423c6c930ed5e32fd5
storm <stooormix@gmail.com>
2022-02-22 20:53:52 +0330

    use state in nav

--- src/component/Layout/headLayout/Nav.js
+++ src/component/Layout/headLayout/Nav.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react'
 
 export default function Nav() {
     
-    
+    const [inputHeadMove, setInputHeadMove] = useState("بخش های سایت")
 
     return (
         <Fragment>
@@ -13,15 +13,15 @@ export default function Nav() {
                             <p>بخش های سایت</p>
                         </div>
                         <div className='select-option'>
-                            <a href='#head-nav' >خانه</a>
-                            <a href='#information' >اطلاعات کلی</a>
-                            <a href='#expertise' >تکنولوژی ها</a>
-                            <a href='#project' >پروژه ها</a>
-                            <a href='#SocialNetwork'>شبکه های اجتماعی</a>
+                            <a href='#head-nav' onClick={()=>setInputHeadMove("خانه")}>خانه</a>
+                            <a href='#information' onClick={()=>setInputHeadMove("اطلاعات کلی")}>اطلاعات کلی</a>
+                            <a href='#expertise' onClick={()=>setInputHeadMove("تکنولوژی ها")}>تکنولوژی ها</a>
+                            <a href='#project' onClick={()=>setInputHeadMove("پروژه ها")}>پروژه ها</a>
+                            <a href='#SocialNetwork' onClick={()=>setInputHeadMove("شبکه های اجتماعی")}>شبکه های اجتماعی</a>
                         </div>
                     </div>
+                    <span>STOoORMX</span>
                 </nav>
-                  
             </div>
         </Fragment>
     )
