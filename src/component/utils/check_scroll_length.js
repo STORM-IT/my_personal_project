10e9b887d0aa5639d747f73735a7ef325d251373
storm <stooormix@gmail.com>
2022-02-24 23:42:02 +0330

    set opacity nav with scroll

--- src/component/utils/check_scroll_length.js
+++ src/component/utils/check_scroll_length.js
@@ -3,6 +3,17 @@ import { useState } from "react";
 window.addEventListener('scroll', () => { Show_footer(window.pageYOffset) });
 export const Show_footer = (Scroll) => {
     console.log(window.pageYOffset)
+    const nav = document.querySelector(".nav-container")
+    if (Scroll >= 400) {
+        
+        nav.classList.remove("hidOpacity")
+        nav.classList.add("opacity")
+    }
+    else{
+        nav.classList.add("hidOpacity")
+        nav.classList.remove("opacity")
+
+    }
     if (Scroll >= 1300) {
         const card = document.querySelectorAll(".card-body > div");
         for (let i = 0; i < card.length; i++) {
@@ -19,7 +30,7 @@ export const Show_footer = (Scroll) => {
             }
         }
     }
-    if (3900<= Scroll) {
+    if (3900 <= Scroll) {
         let line = document.querySelector(".line")
         let logo = document.querySelector(".line img")
         let userName = document.querySelector(".line div")
@@ -29,53 +40,14 @@ export const Show_footer = (Scroll) => {
     }
     const main = document.querySelector("main")
     const moveTopIcon = document.querySelector(".move-top")
-    if (4400<= Scroll) {
-        main.classList.replace("hide_footer_animation","show_footer_animation")
-        moveTopIcon.classList.replace("hideIconTop","showIconTop")
-        // main.classList.add("show_footer_animation")
-        // main.classList.remove("hide_footer_animation")
-        // moveTopIcon.classList.add("showIconTop")
-        // moveTopIcon.classList.remove("hideIconTop")
-        
-    }else if(4400>= Scroll){
+    if (4300 <= Scroll) {
+        moveTopIcon.classList.replace("hideIconTop", "showIconTop")
+        // moveTopIcon.classList.replace("hideIconTop", "showIconTop")
 
-        main.classList.replace("show_footer_animation","hide_footer_animation")
-        moveTopIcon.classList.replace("showIconTop","hideIconTop")
-        // main.classList.add("hide_footer_animation")
-        // main.classList.remove("show_footer_animation")
-        // moveTopIcon.classList.add("hideIconTop")
-        // moveTopIcon.classList.remove("showIconTop")
+    } else {
 
+        main.classList.replace("show_footer_animation", "hide_footer_animation")
+        moveTopIcon.classList.replace("showIconTop", "hideIconTop")
     }
-    // const first=Scroll;
-    // console.log(first)
-    // switch (true) {
-    //     case (1300 <= Scroll):
-    //          // console.log("dds")
-    //     const card = document.querySelectorAll(".card-body > div");
-    //     for (let i = 0; i < card.length; i++) {
-    //         if (i % 2 == 1) {
-    //             setTimeout(() => {
 
-    //                 card[i].classList.add("moveExpertiseLeft");
-    //             }, 700 * i);
-    //         } else {
-    //             setTimeout(() => {
-
-    //                 card[i].classList.add("moveExpertiseRight");
-    //             }, 700 * i);
-    //         }
-    //     }
-    //         break;
-    //     case (3900<= Scroll):
-    //         let line = document.querySelector(".line")
-    //         let logo = document.querySelector(".line img")
-    //         let userName = document.querySelector(".line div")
-    //         line.classList.add("animation_socialNetwork_line")
-    //         logo.classList.add("animation_socialNetwork_logo")
-    //         userName.classList.add("animation_socialNetwork_userName")
-    //         break;
-    //     default:
-    //         break;
-    // }
 }
