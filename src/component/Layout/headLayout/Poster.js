52cc599e5510e31edf5867eabbedd89e4c146f09
storm <stooormix@gmail.com>
2022-02-25 16:04:11 +0330

    fix opacity with scroll from the nav and fixed bug (set opacity from the all tags)

--- src/component/Layout/headLayout/Poster.js
+++ src/component/Layout/headLayout/Poster.js
@@ -32,7 +32,7 @@ export default function Poster() {
     return (
         <Fragment>
             <div className='header-short'>
-                <video id='short-video_head' className='header-short-video' src={url} onEnded={() => next_video()} ref={vidRef} muted />
+                <video id='short-video_head' className='header-short-video' src={url} onEnded={() => next_video()} ref={vidRef} muted poster='./IMG/poster/picture.jpg' />
                 <span>به وبساخت شخصی من خوش آمدید</span>
                 <svg className='header-short-footer' xmlns="http://www.w3.org/2000/svg" viewBox="0 -20 1440 200">
                     <path fill="#393e46" fill-opacity="1" d="M0,96L80,90.7C160,85,320,75,480,90.7C640,107,800,149,960,149.3C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
