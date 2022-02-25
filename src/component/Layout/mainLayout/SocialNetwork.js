faa5e3e9f3ceb40757661e3cc75c1ae39d9d078e
storm <stooormix@gmail.com>
2022-02-25 11:48:59 +0330

    move icon move top in the socialNetwork Layout

--- src/component/Layout/mainLayout/SocialNetwork.js
+++ src/component/Layout/mainLayout/SocialNetwork.js
@@ -3,16 +3,19 @@ import React, { Fragment } from 'react'
 export default function SocialNetwork() {
   return (
     <Fragment>
-       <div id='SocialNetwork' className='nav-space'>
-            </div>
-       <div className='SocialNetwork'>
+      <div className='nav-space'>
+      </div>
+      <div className='SocialNetwork'>
         <div className='line'>
-        <img id='659' src='./IMG/logo_lessen/github.svg'/>
-        <div>
-        <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>STOORMIX</a>
+          <img id='659' src='./IMG/logo_lessen/github.svg' />
+          <div>
+            <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>STOORMIX</a>
+          </div>
         </div>
-        </div>
-    </div>
+        <a href='#head-nav' className='move-top'>
+          <img src='./IMG/Move_logo/up.svg' className='move-top-icon' />
+        </a>
+      </div>
     </Fragment>
   )
 }
