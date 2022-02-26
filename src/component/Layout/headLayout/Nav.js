927eb81f7c3c4dc448694ff0d593ad0a354f9815
storm <stooormix@gmail.com>
2022-02-27 01:51:35 +0330

    create maximizing effect header text

--- src/component/Layout/headLayout/Nav.js
+++ src/component/Layout/headLayout/Nav.js
@@ -40,7 +40,7 @@ export default function Nav() {
                         </div>
                         ):null}
                     </div>
-                    <div><span>S</span><span>T</span><span>O</span><span>O</span><span>O</span><span>R</span><span>M</span><span>I</span><span>X</span></div>
+                    <div className='div_text_head'><span>S</span><span>T</span><span>O</span><span>O</span><span>O</span><span>R</span><span>M</span><span>I</span><span>X</span></div>
                 </nav>
             </div>
         </Fragment>
