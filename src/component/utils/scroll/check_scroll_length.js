26f87c006ff13bdf7903f702f0322f87f3ec8484
storm <stooormix@gmail.com>
2022-02-26 23:41:13 +0330

    create text effect utils scroll

--- src/component/utils/scroll/check_scroll_length.js
+++ src/component/utils/scroll/check_scroll_length.js
@@ -4,15 +4,19 @@ import { scroll_footer } from "./footer";
 import { scroll_img_background } from "./imgBackground";
 import { scroll_nav } from "./nav";
 import { scroll_sociaNetwork } from "./socialNetwork";
+import { scroll_text_head } from "./textHead";
 
 window.addEventListener('scroll', () => { scroll_utils(window.pageYOffset) });
+var LastScroll=0;
 export const scroll_utils = (Scroll) => {
-
-    console.log(window.pageYOffset)
+    
+    // console.log(LastScroll,Scroll);
+    // console.log(window.pageYOffset)
     scroll_nav(Scroll); // Scroll >= 600
+    scroll_text_head(Scroll,LastScroll);
     scroll_img_background(Scroll);// Scroll <= 1300 || 3900 <= Scroll
     scroll_card_expertise(Scroll);// Scroll > 1300
     scroll_sociaNetwork(Scroll); // Scroll >= 3900
     scroll_footer(Scroll);// Scroll >= 4300
-    
+    LastScroll=Scroll;
 }
---
+++ src/component/utils/scroll/textHead.js
@@ -0,0 +1,13 @@
+export const scroll_text_head=(Scroll,LastScroll)=>{
+    if (Scroll >= 600) {
+        if(Scroll > LastScroll){
+
+        }
+        else{
+            
+        }
+    }
+    else {
+        
+    }
+}
