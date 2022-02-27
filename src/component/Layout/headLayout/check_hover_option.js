ad6eaa038d8181df8c1bd0965eb23e0e4f8cc3e2
storm <stooormix@gmail.com>
2022-02-27 19:36:39 +0330

    fix bug div option in the head nav

--- src/component/Layout/headLayout/check_hover_option.js
+++ src/component/Layout/headLayout/check_hover_option.js
@@ -7,6 +7,7 @@ export const hover_option = () => {
 
         div_option.classList.remove("hide_option_select_nav")
         div_option.classList.add("visible")
+        // alert("d")
     }
 
     div_select.addEventListener('mouseenter', () => {
@@ -19,18 +20,11 @@ export const hover_option = () => {
         div_option.classList.replace("show_option_select_nav", "hide_option_select_nav")
     })
 
-    div_option.addEventListener('mouseenter', option_mouseEnter)
+    div_option.addEventListener('mouseenter',()=> option_mouseEnter())
 
 
     div_option.addEventListener('mouseleave', () => {
         div_option.removeEventListener('mouseenter', option_mouseEnter)
         div_option.classList.add("hide_option_select_nav")
     })
-    // div_option.addEventListener('mouseenter', option_mouseEnter)
-
-
-    // div_option.addEventListener('mouseleave', () => {
-    //     div_option.removeEventListener('mouseenter', option_mouseEnter)
-    //     div_option.classList.add("hide_option_select_nav")
-    // })
 }
