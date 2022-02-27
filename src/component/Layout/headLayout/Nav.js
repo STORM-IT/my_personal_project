4514ad20456f8b519d5b639ef92380f8627cb893
storm <stooormix@gmail.com>
2022-02-27 12:58:35 +0330

    show option in nav (fix z index and div name)

--- src/component/Layout/headLayout/Nav.js
+++ src/component/Layout/headLayout/Nav.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 
 export default function Nav() {
-    
+
     const [inputHeadMove, setInputHeadMove] = useState("بخش های سایت")
     const [showNavOption, setshowNavOption] = useState(false)
     // const [scrollTop, setScrollTop] = useState(window.pageYOffset)
@@ -11,13 +11,13 @@ export default function Nav() {
     //     setScrollTop(window.pageYOffset)
     //     console.log(scrollTop)
     // }, [scrollTop])
-    
-    const show_select_option_nav=()=>{
+
+    const show_select_option_nav = () => {
         setshowNavOption(!showNavOption);
         console.log(showNavOption)
     }
 
-    const show_footer=()=>{
+    const show_footer = () => {
 
     }
 
@@ -27,18 +27,18 @@ export default function Nav() {
                 <nav className='nav-container'>
                     <div className='select'>
                         <div className='select-input'>
-                            <input onClick={()=>show_select_option_nav()} value={showNavOption?inputHeadMove:"بخش های سایت"} readOnly></input>
+                            <input onClick={() => show_select_option_nav()} value={showNavOption ? inputHeadMove : "بخش های سایت"} readOnly></input>
                         </div>
-                        {showNavOption?(
+                        {showNavOption ? (
 
-                        <div className='select-option'>
-                            <a href='#head-nav' onClick={()=>setInputHeadMove("خانه")}>خانه</a>
-                            <a href='#information' onClick={()=>setInputHeadMove("اطلاعات کلی")}>اطلاعات کلی</a>
-                            <a href='#expertise' onClick={()=>setInputHeadMove("تکنولوژی ها")}>تکنولوژی ها</a>
-                            <a href='#project' onClick={()=>setInputHeadMove("پروژه ها")}>پروژه ها</a>
-                            <a href='#SocialNetwork' onClick={()=>setInputHeadMove("شبکه های اجتماعی")}>شبکه های اجتماعی</a>
-                        </div>
-                        ):null}
+                            <div className='select-option'>
+                                <a href='#head-nav' onClick={() => setInputHeadMove("خانه")}>خانه</a>
+                                <a href='#information' onClick={() => setInputHeadMove("اطلاعات کلی")}>اطلاعات کلی</a>
+                                <a href='#expertise' onClick={() => setInputHeadMove("تکنولوژی ها")}>تکنولوژی ها</a>
+                                <a href='#project' onClick={() => setInputHeadMove("پروژه ها")}>پروژه ها</a>
+                                <a href='#SocialNetwork' onClick={() => setInputHeadMove("شبکه های اجتماعی")}>شبکه های اجتماعی</a>
+                            </div>
+                        ) : null}
                     </div>
                     <div className='div_text_head'><span>S</span><span>T</span><span>O</span><span>O</span><span>O</span><span>R</span><span>M</span><span>I</span><span>X</span></div>
                 </nav>
--- src/component/utils/scroll/textHead.js
+++ src/component/utils/scroll/textHead.js
@@ -1,22 +1,22 @@
-export const scroll_text_head=(Scroll,LastScroll)=>{
+export const scroll_text_head = (Scroll, LastScroll) => {
     const div_text_head = document.querySelector(".div_text_head")
     if (Scroll >= 600) {
-        if(Scroll >= LastScroll){
+        if (Scroll >= LastScroll) {
             for (let i = 1; i <= 9; i++) {
                 // const span = document.querySelector(`div span:nth-child(${i}) `)
                 div_text_head.classList.add("minimizing_text_head")
-                div_text_head.classList.replace("maximizing_text_head","minimizing_text_head")
+                div_text_head.classList.replace("maximizing_text_head", "minimizing_text_head")
             }
         }
-        else{
+        else {
             div_text_head.classList.add("maximizing_text_head")
-            div_text_head.classList.replace("minimizing_text_head","maximizing_text_head")
-            
+            div_text_head.classList.replace("minimizing_text_head", "maximizing_text_head")
+
         }
     }
     else {
         div_text_head.classList.add("maximizing_text_head")
-        div_text_head.classList.replace("minimizing_text_head","maximizing_text_head")
-        
+        div_text_head.classList.replace("minimizing_text_head", "maximizing_text_head")
+
     }
 }
