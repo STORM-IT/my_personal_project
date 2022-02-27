554d29fb9dc2a713211f390ba6de252f526d54d2
storm <stooormix@gmail.com>
2022-02-27 13:17:32 +0330

    fix bug (action hide footer effect with reload site)

--- src/component/utils/scroll/footer.js
+++ src/component/utils/scroll/footer.js
@@ -4,14 +4,19 @@ export const scroll_footer = (Scroll)=>{
     if ( Scroll >= 4300 ) {
         moveTopIcon.classList.add("showIconTop")
         moveTopIcon.classList.replace("hideIconTop", "showIconTop")
-
-        main.classList.add("hide_footer_animation")
-        main.classList.replace("hide_footer_animation", "show_footer_animation")
-    } else {
-
-
+        
+        // main.classList.replace("hide_footer_animation", "show_footer_animation")
         main.classList.add("show_footer_animation")
-        main.classList.replace("show_footer_animation", "hide_footer_animation")
+        main.classList.replace("hide_footer_animation","show_footer_animation" )
+        
+    } else {
+        if(main.classList.contains("show_footer_animation")){
+            main.classList.add("hide_footer_animation")
+            main.classList.replace("show_footer_animation","hide_footer_animation" )
+
+            // main.classList.replace("show_footer_animation", "hide_footer_animation")
+        }
+
 
 
         moveTopIcon.classList.add("hideIconTop")
