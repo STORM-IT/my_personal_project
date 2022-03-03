742d343c96a32f63d05f8534957c56965f23a3f5
storm <stooormix@gmail.com>
2022-03-03 18:10:21 +0330

    add shop project

---
+++ src/Projects/javascript/heads_or_tails/heads_or_tails.js
@@ -0,0 +1,27 @@
+const score=[0,0];
+var select_pc=['shir','khat'];
+var time1;
+function check(point) {
+    debugger
+    var number_random=Math.floor(Math.random()*2);
+    
+    
+    if (point===select_pc[number_random]) {
+        clearTimeout(time1);
+       document.getElementById('p-top').innerHTML="your win";
+       score[0]++; 
+       document.getElementById('USER').innerHTML=score[0].toString();
+       document.getElementById('div-button').style.backgroundColor="green";
+       time1 = setTimeout(clear_1,2000);
+    } else {
+        clearTimeout(time1);
+        document.getElementById('p-top').innerHTML="your filed"; 
+        score[1]++;
+        document.getElementById('PC').innerHTML=score[1].toString(); 
+        document.getElementById('div-button').style.backgroundColor="red";
+        time1 = setTimeout(clear_1,2000);
+    }
+    function clear_1() {
+        document.getElementById('div-button').style.backgroundColor="white";
+    }
+}
