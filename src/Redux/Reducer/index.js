5404d7414e1f90827a32efa913fbc0d05b3f9c21
storm <stooormix@gmail.com>
2022-03-06 14:11:52 +0330

    import findCorrectNumber in index reducer and set redux thunk in store

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { findCorrectNumber } from "./Projects/find_correct_number";
 import {urlVideoReducer} from './short_video'
 export const root= combineReducers({
-    url:urlVideoReducer
+    url:urlVideoReducer,
+    correctNumber:findCorrectNumber
 })
--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
-import {root} from '../Reducer/index'
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk';
+import { root } from '../Reducer/index'
 
-export const store=createStore(root,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(root, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 store.subscribe(() => console.log(store.getState()))
