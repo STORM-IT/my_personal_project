7dc112427df8abec9346e9ae3bb90dc3c41f6297
storm <stooormix@gmail.com>
2022-03-06 13:43:13 +0330

    remove redux in component

--- src/component/Redux/Reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import {urlVideoReducer} from './short_video'
-export const root= combineReducers({
-    url:urlVideoReducer
-})
--- src/component/Redux/Reducer/short_video.js
+++
@@ -1,9 +0,0 @@
-export const urlVideoReducer = (state = "./IMG/head_short_video/computer-code-in-the-screen.mp4", action) => {
-
-    switch (action.type) {
-      case "SET-URL": 
-        return action.payload;
-        
-      default: return state;
-    }
-  }
