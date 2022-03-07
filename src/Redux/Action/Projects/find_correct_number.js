88f4a57ead5ecc552cd0b3a364e3f63fd5edfb02
storm <stooormix@gmail.com>
2022-03-07 14:05:37 +0330

    add clear and start game in reducer (find correct number) / set btn text and change start_game value with true and set dispatch

--- src/Redux/Action/Projects/find_correct_number.js
+++ src/Redux/Action/Projects/find_correct_number.js
@@ -1,43 +1,48 @@
+import { batch } from "react-redux";
 
 export const increment = (index) => {
     return (dispatch, getState) => {
-        let state_correctNumber = {...getState().correctNumber};
+        let state_correctNumber = { ...getState().correctNumber };
         var numbers = [...state_correctNumber.num_input];
         numbers[index]++;
-        state_correctNumber.num_input=numbers;
-        dispatch({ type: "INCREMENT", payload:state_correctNumber });
+        state_correctNumber.num_input = numbers;
+        dispatch({ type: "INCREMENT", payload: state_correctNumber });
     }
 }
 
 
 export const decrement = (index) => {
     return (dispatch, getState) => {
-        let state_correctNumber = {...getState().correctNumber};
+        let state_correctNumber = { ...getState().correctNumber };
         var numbers = [...state_correctNumber.num_input];
         numbers[index]--;
-        state_correctNumber.num_input=numbers;
-        dispatch({ type: "DECREMENT", payload:state_correctNumber });
+        state_correctNumber.num_input = numbers;
+        dispatch({ type: "DECREMENT", payload: state_correctNumber });
     }
 }
 
 export const setValueWithWrite = (index, value) => {
     return (dispatch, getState) => {
-        var state_correctNumber = {...getState().correctNumber};
+        var state_correctNumber = { ...getState().correctNumber };
         var numbers = [...state_correctNumber.num_input];
         numbers[index] = value == "" ? value : value == "-" ? "-" : parseInt(value);
-        state_correctNumber.num_input=numbers;
-        dispatch({ type: "SET_VALUE", payload:state_correctNumber });
+        state_correctNumber.num_input = numbers;
+        dispatch({ type: "SET_VALUE", payload: state_correctNumber });
     }
 }
-// export const clearValueWithWrite = () => {
-//     // return (dispatch, getState) => {
+export const clearValueWithWrite = () => {
+    return (dispatch, getState) => {
 
-        
-//         return{ type: "CLEAR", payload: [0,0,0,0,0] };
-//     // }
-// }
+        var state_correctNumber = { ...getState().correctNumber };
+        // var numbers = [...state_correctNumber.num_input];
+        // numbers[index]++;
+        const state = { num_input: [0, 0, 0, 0, 0], check_start: false, button_text: "" }
+        state_correctNumber.num_input = state;
+        dispatch({ type: "INCREMENT", payload: state_correctNumber });
+    }
+}
 
-var check_start = false;
+// var check_start = false;
 var button;
 var list_input;
 var list_text_math;
@@ -45,7 +50,12 @@ var effort;
 var effort_check = 0;
 export const get_number = () => {
     return (dispatch, getState) => {
-        const state=getState().correctNumber
+        // let {check_start} =  getState().correctNumber ;
+        let correctNumber = { ...getState().correctNumber };
+        var check_start = correctNumber.check_start;
+        // dispatch({ type: "BTN_TEXT" });
+        // debugger
+        
         button = document.getElementById('b-check');
         list_input = document.querySelectorAll(".input-number");
         list_text_math = document.querySelectorAll(".math-text");
@@ -55,22 +65,24 @@ export const get_number = () => {
         for (let i = 0; i < 5; i++) {
             const length_math = Math.floor(Math.random() * 2 + 1)
             var number = Math.floor(Math.random() * 9 + 1);
-    
+            
             for (let i = 0; i < length_math; i++) {
                 number += `${list_operator_math[Math.floor(Math.random() * 3)]} ${Math.floor(Math.random() * 10)}`
             }
             list_math.push(number);
         }
-    
+        
         if (check_start === false) {
+
             for (let i = 0; i < 5; i++) {
                 list_text_math[i].innerHTML = list_math[i].replaceAll(" * ", " x ");
                 list_text_math[i].classList.add("show_animation_text_math")
-                list_text_math[i].classList.replace("hide_animation_text_math","show_animation_text_math");
+                list_text_math[i].classList.replace("hide_animation_text_math", "show_animation_text_math");
                 list_input[i].correct = eval(list_math[i]);
             }
-            check_start = true;
-            button.innerHTML = 'check number';
+            correctNumber.check_start=true;
+            correctNumber.button_text="check number";
+            dispatch({ type: "START_GAME" , payload:correctNumber});
         }
         else {
             var number_all = document.querySelectorAll('.input-number');
@@ -87,24 +99,23 @@ export const get_number = () => {
                     number_all[i].style.background = 'green';
                     win++;
                 }
-    
+
             }
-            if (win === 1) {
+            if (win === 2) {
                 button.innerHTML = 'start game';
-                for (let i = 0; i < 5; i++) {       
+                for (let i = 0; i < 5; i++) {
                     list_text_math[i].classList.add("hide_animation_text_math");
-                    list_text_math[i].classList.replace("show_animation_text_math","hide_animation_text_math");
-                    
+                    list_text_math[i].classList.replace("show_animation_text_math", "hide_animation_text_math");
+
                 }
                 alert(`your effort is : ${effort}`)
-                effort=0;
+                effort = 0;
                 check_start = false;
                 list_math = [];
                 win = 0;
-                return{ type: "CLEAR", payload: [0,0,0,0,0] };
+                dispatch({ type: "CLEAR" });
             }
-            
+
         }
-        return{ type: "CLEAR", payload: [0,0,0,0,0] };
     }
 }
--- src/Redux/Reducer/Projects/find_correct_number.js
+++ src/Redux/Reducer/Projects/find_correct_number.js
@@ -1,14 +1,16 @@
 // export const findCorrectNumber = (state = {num_input:[0, 0, 0, 0, 0], check_start:false}, action) => {
-export const findCorrectNumber = (state = {num_input:[0, 0, 0, 0, 0], check_start:false}, action) => {
+export const findCorrectNumber = (state = {num_input:[0, 0, 0, 0, 0], check_start:false , button_text:"start game"}, action) => {
 
   switch (action.type) {
     case "SET_VALUE":
       return {...action.payload};
       case "CLEAR":
-        return {...action.payload};
+        return {num_input:[0, 0, 0, 0, 0], check_start:false , button_text:""};
     case "INCREMENT":
       return {...action.payload};
     case "DECREMENT":
+      return {...action.payload};
+    case "START_GAME":
       return {...action.payload};
 
     default: return state;
