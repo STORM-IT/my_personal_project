f7e74015370ced7a1061b2900ac4bec1403546d3
storm <stooormix@gmail.com>
2022-03-10 11:00:51 +0330

    responsive header video/resnosive leaves/remove card project/decrement width/height sociaNetwork card

--- src/component/Layout/mainLayout/Project.js
+++ src/component/Layout/mainLayout/Project.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Project() {
     return (
         <Fragment>
-            <div id='project' className='nav-space'>
-            </div>
+            {/* <div id='project' className='nav-space'>
+            </div> */}
             <div className='img_project_container'>
                 <div className='info_layout_css'>
 
@@ -13,14 +13,14 @@ export default function Project() {
                     <div className='text_img_container'>
                         <h3>پروژه ها</h3>
                         <hr />
-                        <p className='glowIn_text_project'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                        <p className='glowIn_text_project'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در سزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
 
                     </div>
                     <img id='img_project' src='./IMG/img_design/pngwing.com.png' />
                 </div>
                 <section className='project'>
 
-                    <div className='project-container'>
+                    {/* <div className='project-container'>
 
                         <div className='project-container-project-1'>
                             <Link className='badge-file' to={"/project"}>HTML CSS</Link>
@@ -52,7 +52,7 @@ export default function Project() {
                             <div></div>
                         </div>
 
-                    </div>
+                    </div> */}
                     {/* <img src="https://www.transparentpng.com/thumb/thunderstorm/thunder-light-thunderstorm-png-18.png" alt="thunder light thunderstorm png @transparentpng.com" /> */}
                 </section>
             </div>
--- src/component/Layout/mainLayout/SocialNetwork.js
+++ src/component/Layout/mainLayout/SocialNetwork.js
@@ -5,19 +5,19 @@ export default function SocialNetwork() {
     <Fragment>
       {/* <div className='nav-space'>
       </div> */}
-      <div className='hiddens'>
-
       <div className='SocialNetwork'>
-        <div className='line'>
-          <img id='659' src='./IMG/logo_lessen/github.svg' />
-          <div>
-            <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>STOORMIX</a>
+
+        <div className='SocialNetwork-container'>
+          <div className='line'>
+            <img id='659' src='./IMG/logo_lessen/github.svg' />
+            <div>
+              <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>STOORMIX</a>
+            </div>
           </div>
+          <a href='#head-nav' className='move-top'>
+            <img src='./IMG/Move_logo/up.svg' className='move-top-icon' />
+          </a>
         </div>
-        <a href='#head-nav' className='move-top'>
-          <img src='./IMG/Move_logo/up.svg' className='move-top-icon' />
-        </a>
-      </div>
       </div>
     </Fragment>
   )
