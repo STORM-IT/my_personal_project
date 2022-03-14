fcb7ef65b547a86ec77c079ff020021378704f8d
storm <stooormix@gmail.com>
2022-03-14 15:40:47 +0330

    change name class in expertise layout / change style filter project card

--- src/component/Layout/mainLayout/Expertise.js
+++ src/component/Layout/mainLayout/Expertise.js
@@ -17,68 +17,68 @@ export default function Expertise() {
         </div>
         <img id='img_expertise' src='./IMG/img_design/expertise_img.png' />
       </div>
-      <section className='card'>
-        <div className='card-container' >
-          <div className='card-text'>
+      <section className='Card'>
+        <div className='Card-container' >
+          <div className='Card-text'>
             <h3>تکنولوژی های آموزش دیده</h3>
           </div>
-          <div className='card-expertise'>
-            <div className='card-body'>
-              <div className='card-expertise-html vc'>
+          <div className='Card-expertise'>
+            <div className='Card-body'>
+              <div className='Card-expertise-html vc'>
                 <span>HTML</span>
                 <img src='./IMG/logo_lessen/html.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div id='card-expertise-css1' className='card-expertise-css vc'>
+            <div className='Card-body'>
+              <div id='Card-expertise-css1' className='Card-expertise-css vc'>
                 <span>CSS</span>
                 <img src='./IMG/logo_lessen/css.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-javascript'>
+            <div className='Card-body'>
+              <div className='Card-expertise-javascript'>
                 <span>JAVASCTIPT</span>
                 <img src='./IMG/logo_lessen/javascript.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-bootstrap'>
+            <div className='Card-body'>
+              <div className='Card-expertise-bootstrap'>
                 <span>BOOTSTRAP</span>
                 <img src='./IMG/logo_lessen/bootstrap.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-jquery'>
+            <div className='Card-body'>
+              <div className='Card-expertise-jquery'>
                 <span>JQUERY</span>
                 <img src='./IMG/logo_lessen/jquery.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-sass'>
+            <div className='Card-body'>
+              <div className='Card-expertise-sass'>
                 <span>SASS</span>
                 <img src='./IMG/logo_lessen/sass.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-react'>
+            <div className='Card-body'>
+              <div className='Card-expertise-react'>
                 <span>REACT</span>
                 <img src='./IMG/logo_lessen/react.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-redux'>
+            <div className='Card-body'>
+              <div className='Card-expertise-redux'>
                 <span>REDUX</span>
                 <img src='./IMG/logo_lessen/redux.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-git'>
+            <div className='Card-body'>
+              <div className='Card-expertise-git'>
                 <span>GIT</span>
                 <img src='./IMG/logo_lessen/git.svg' />
               </div>
             </div>
-            <div className='card-body'>
-              <div className='card-expertise-sharp'>
+            <div className='Card-body'>
+              <div className='Card-expertise-sharp'>
                 <span>C#</span>
                 <img src='./IMG/logo_lessen/c_sharp.svg' />
               </div>
