b594d1fbe753b75ec2e87712071ed96f491fa05d
storm <stooormix@gmail.com>
2022-03-18 15:22:03 +0330

    set html for find correct text

--- src/Projects/javascript/find_correct_text/find_correct_text.js
+++ src/Projects/javascript/find_correct_text/find_correct_text.js
@@ -1,67 +1,16 @@
-var message=document.getElementById('txt-top');
-var container=document.querySelectorAll('div');
-var button=document.getElementById('check');
-var list_effort=[];
-var check_start=false;
-var list_name=['ali','ahmad','reza'];
-var list_name_Broken=['lia','madah','zare'];
-var effort=0;
-var select=0;
+import React, { Fragment } from 'react'
+import get_random_text from './script_find_correct_text'
 
-function challenge() {
-
-    debugger
-    if(check_start===false){
-        select=Math.floor(Math.random()*3);
-        var element=document.createElement('p');
-        element.innerHTML=list_name_Broken[select];
-        element.style.fontSize='30px';
-        container[0].appendChild(element);
-        check_start=true;
-        message.innerHTML='your effort is : '+ effort;
-    }
-    else{
-        
-        //effort++;
-        message.innerHTML='your effort is : '+ ++effort;
-
-        var txt=document.getElementById('input-txt').value;
-        if(txt===list_name[select]){
-
-            document.getElementById('input-txt').style.backgroundColor='green';
-            message.innerHTML='your win in '+ effort +' effort';
-            list_effort.push('name is :' + list_name[select] + '<br>'+ 'your effort is :' + effort + '<hr>');
-            
-
-            let txt=document.createElement('p');
-            txt.innerHTML='name is :' + list_name[select] + '<br>'+ 'your effort is :' + effort + '<hr>';
-                
-            container[1].appendChild(txt);
-            clear();
-            setTimeout(clear_color,3000);
-            
-
-        }else{
-            document.getElementById('input-txt').style.backgroundColor='red';
-            setTimeout(clear_color,2000);
-        }
-
-    }
-
-    function clear() {
-        
-        
-            effort=0;
-            select=0
-            check_start=false;
-            document.getElementById('input-txt').value = '';
-            container[0].innerHTML='';
-            button.innerHTML='start game';
-
-    }
-    function clear_color() {
-        document.getElementById('input-txt').style.backgroundColor='White';
-
-}
-    
+export default function Find_correct_text() {
+    return (
+        <Fragment>
+            {/* <div id='find_correct_text'> */}
+                <p id="txt-top">you are ready new challenge?</p>
+                <div id="container-txt"></div>
+                <input type="text" name="" id="input-txt" /> <br />
+                <button id="check" onClick={() => {get_random_text() }}>start game</button>
+                <div id="effore"></div>
+            {/* </div> */}
+        </Fragment>
+    )
 }
