c8d550245365fed4fd60d8d36fa51ba217320972
storm <stooormix@gmail.com>
2022-04-03 16:12:51 +0430

    response calendar project

--- src/Projects/calendar/Header/Header.jsx
+++ src/Projects/calendar/Header/Header.jsx
@@ -19,11 +19,11 @@ const Header = () => {
     return (<SimpleContext.Consumer>
         {context => (
             <form action='#' onSubmit={event => event.preventDefault()} className="box_obj">
-                <Alert className='' id='Close'>
-                    <p> Please write your information and click <kbd className="bg-success">Add My Program</kbd></p>
+                <Alert className='alert_title' id='Close'>
+                    <p> Please write your information and click <kbd className="bg-success h-25">Add My Program</kbd></p>
                 </Alert>
                 <br />
-                <Badge pill bg='light' text='dark' className='p-2 display-3 mb-5' >Youre lenght list program is <Badge bg={Color_Badge}>{context.ListPersons.length}</Badge> 👀</Badge>
+                <Badge pill bg='light' text='dark' className='p-2 display-3 ' >Youre lenght list program is <Badge bg={Color_Badge}>{context.ListPersons.length}</Badge> 👀</Badge>
                 <br />
                 <div >
                     <div id='div_text_style'>
@@ -34,7 +34,6 @@ const Header = () => {
                         <span className="span_text_style left"></span>
                     </div>
                 </div>
-                <br />
                 <div >
                     <div id='div_text_style'>
                         <textarea id='input_text_style' className="Clear description_program" onChange={event => { context.set_name_age({ age: event.target.value }) }} placeholder="please type your program" >
@@ -46,10 +45,10 @@ const Header = () => {
                     </div>
                 </div>
                 <br />
-                <Button type='submit' id='btn_submit' onClick={context.add_persons} className=" mb-5 mt-2">Add My Program</Button>
+                <Button type='submit' id='btn_submit' onClick={context.add_persons} className="mt-1">Add My Program</Button>
                 <br />
                 <br />
-                <Button onClick={context.show_or_hiden} className={context.showList ? "btn-warning" : "btn-info"}>
+                <Button onClick={context.show_or_hiden} className={`${context.showList ? "btn-warning" : "btn-info"} btn_toggle_show`}>
                     {context.showList ? "HIDEN" : "SHOW"}
                 </Button>
                 <hr />
