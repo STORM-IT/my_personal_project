d3a8a8ee6747a0b04628e8b8ecd675aa4685ae1b
storm <stooormix@gmail.com>
2022-04-11 22:04:53 +0430

    add title icon for tab / change link github in socialNetwork layout

--- src/component/Layout/mainLayout/SocialNetwork.js
+++ src/component/Layout/mainLayout/SocialNetwork.js
@@ -11,7 +11,7 @@ export default function SocialNetwork() {
           <div className='line'>
             <img id='659' src='./IMG/logo_lessen/github.svg' />
             <div>
-              <a href='https://github.com/STORM-IT' target={"_blank"} className='Username'>STOORMIX</a>
+              <a href='https://github.com/alipsm' target={"_blank"} className='Username'>STOORMIX</a>
             </div>
           </div>
           <a href='#head-nav' className='move-top'>
