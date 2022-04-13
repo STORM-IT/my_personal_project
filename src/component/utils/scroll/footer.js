c62a5dfc9ae8082f53c6e6b62f7acb3b27b23dbf
storm <stooormix@gmail.com>
2022-04-13 13:40:41 +0430

    remove height in btn move top with animation and scroll top

--- src/component/utils/scroll/footer.js
+++ src/component/utils/scroll/footer.js
@@ -3,7 +3,6 @@ import { scroll_sociaNetwork } from "./socialNetwork"
 export const scroll_footer = (Scroll,innerWidth)=>{
     const main = document.querySelector(".SocialNetwork-container")
     const moveTopIcon = document.querySelector(".move-top")
-    // if ( Scroll >= 3790 & innerWidth <=375 || Scroll >= 3810 && innerWidth >=376 & innerWidth <=490 || Scroll >= 4340 && innerWidth >=491 & innerWidth <=600 || Scroll >= 4345 && innerWidth <=601 & innerWidth >=768 || Scroll >= 5330 && innerWidth >=769 & innerWidth <=1000  || Scroll >= 5550 & innerWidth >=1001) {
     if ( Scroll >= 3790 & innerWidth <=375 || Scroll >= 3810 && innerWidth >=376 & innerWidth <=490 || Scroll >= 4670 && innerWidth >=491 & innerWidth <=600 || Scroll >= 5210 && innerWidth >=601 & innerWidth <=768 || Scroll >= 5330 && innerWidth >=769 & innerWidth <=1000  || Scroll >= 5550 & innerWidth >=1001) {
         moveTopIcon.classList.add("showIconTop")
         moveTopIcon.classList.replace("hideIconTop", "showIconTop")
@@ -16,9 +15,9 @@ export const scroll_footer = (Scroll,innerWidth)=>{
         if(main.classList.contains("socialNetwork_move_top_class")){
             main.classList.add("socialNetwork_move_bottom_class")
             main.classList.replace("socialNetwork_move_top_class","socialNetwork_move_bottom_class" )
+            
+            moveTopIcon.classList.add("hideIconTop")
+            moveTopIcon.classList.replace("showIconTop", "hideIconTop")
         }
-
-        moveTopIcon.classList.add("hideIconTop")
-        moveTopIcon.classList.replace("showIconTop", "hideIconTop")
     }
 }
