99baec8e21ac2b2d745847653a829701f1f7297f
storm <stooormix@gmail.com>
2022-04-14 23:23:50 +0430

    useing HashRouter for the fixed route path and remove browserRouter

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import {store} from './Redux/Store/store'
 import App from './App';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-render(<Provider store={store}><App/></Provider>,document.getElementById("root"));
+import { HashRouter as Router } from 'react-router-dom';
+render(<Router><Provider store={store}><App/></Provider></Router>,document.getElementById("root"));
