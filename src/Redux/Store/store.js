830106260a413c205ef100913412fc99624e3cff
storm <stooormix@gmail.com>
2022-04-14 23:35:28 +0430

    remove link extension redux devtools because ready project for the publish

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -3,7 +3,11 @@ import thunk from 'redux-thunk';
 import { getAllProjects } from '../Action/filter_project';
 import { root } from '../Reducer/index'
 
-export const store = createStore(root, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+// link extension redux devtools
+//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+export const store = createStore(root, compose(applyMiddleware(thunk)));
 
 store.dispatch(getAllProjects())
-store.subscribe(() => console.log(store.getState()))
+// store.subscribe(() => console.log(store.getState()))
