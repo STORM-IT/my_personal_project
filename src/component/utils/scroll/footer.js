02ba50c67159a15a9602fff0498e5133eca66e06
storm <stooormix@gmail.com>
2022-04-14 23:29:35 +0430

    fixing show animation foooter and glowin with scroll

--- src/component/utils/scroll/footer.js
+++ src/component/utils/scroll/footer.js
@@ -5,7 +5,7 @@ export const scroll_footer = (Scroll,innerWidth)=>{
     const moveTopIcon = document.querySelector(".move-top")
     if (main) {
         
-        if ( Scroll >= 3790 & innerWidth <=375 || Scroll >= 3810 && innerWidth >=376 & innerWidth <=490 || Scroll >= 4670 && innerWidth >=491 & innerWidth <=600 || Scroll >= 5210 && innerWidth >=601 & innerWidth <=768 || Scroll >= 5330 && innerWidth >=769 & innerWidth <=1000  || Scroll >= 5550 & innerWidth >=1001) {
+        if ( Scroll >= 4550 & innerWidth <=375 || Scroll >= 4580 && innerWidth >=376 & innerWidth <=490 || Scroll >= 4670 && innerWidth >=491 & innerWidth <=600 || Scroll >= 5210 && innerWidth >=601 & innerWidth <=768 || Scroll >= 5330 && innerWidth >=769 & innerWidth <=1000  || Scroll >= 5550 & innerWidth >=1001) {
             moveTopIcon.classList.add("showIconTop")
             moveTopIcon.classList.replace("hideIconTop", "showIconTop")
             
--- src/component/utils/scroll/set_glowIn.js
+++ src/component/utils/scroll/set_glowIn.js
@@ -3,9 +3,8 @@ import { show_text_information } from "../glowIn_text"
 var List_Information=[];
 export const set_effect_glowIn = (Scroll,innerWidth) => {
 
-    if (Scroll > 500 & Scroll < 1300 & innerWidth >=769 || Scroll > 130 & Scroll < 600 & innerWidth <=768) {
+    if (Scroll > 500 & Scroll < 1300 & innerWidth >=769 || Scroll > 400 & Scroll < 600 & innerWidth <=768) {
         let find=List_Information.find(e=>e=="info")
-        // debugger
         if(!find){
             let img=document.querySelector("#img_info")
             img.classList.add("img_brightness_class")
@@ -13,24 +12,4 @@ export const set_effect_glowIn = (Scroll,innerWidth) => {
             show_text_information("info")
         }
     }
-    // if (Scroll > 1100 && Scroll < 1900) {
-    //     // alert("start")
-    //     let find=List_Information.find(e=>e=="expertise")
-    //     if(!find){
-    //         let img=document.querySelector("#img_expertise")
-    //         img.classList.add("img_brightness_class")
-    //         List_Information.push("expertise")
-    //         show_text_information("expertise")
-    //     }
-    // }
-    // if (Scroll > 3700 && Scroll < 4500) {
-    //     // alert("start")
-    //     let find=List_Information.find(e=>e=="project")
-    //     if(!find){
-    //         let img=document.querySelector("#img_project")
-    //         img.classList.add("img_brightness_lamp_class")
-    //         List_Information.push("project")
-    //         show_text_information("project")
-    //     }
-    // }
 }
