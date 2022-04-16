e42c10bf7d3f23e641f4c56edfe4c608b139c3a5
storm <stooormix@gmail.com>
2022-04-16 23:48:07 +0430

    set next line in text

--- src/component/utils/glowIn_text.js
+++ src/component/utils/glowIn_text.js
@@ -1,17 +1,31 @@
 
-
 export const show_text_information = (layout) => {
 
-    let glowInText = document.querySelector(`.glowIn_text_${layout}`);
-    glowInText.classList.add("visible");
-    // glowInTexts.forEach(glowInText => {
-      let letters = glowInText.textContent.split("");
-      glowInText.textContent = "";
-      letters.forEach((letter, i) => {
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.style.animationDelay = `${i * 0.019}s`;
-        glowInText.append(span);
-      });
-    // });
-  }
+  let glowInText = document.querySelector(`.glowIn_text_${layout}`);
+  glowInText.classList.add("visible");
+
+  let letters = glowInText.textContent.split("");
+  glowInText.textContent = "";
+  letters.forEach((letter, i) => {
+
+    let span = document.createElement("span");
+    let p = document.createElement("p");
+
+
+    span.style.animationDelay = `${i * 0.019}s`;
+    span.style.animationDelay = `${i * 0.019}s`;
+
+    span.textContent = letter;
+    if (letter == ".") {
+
+      p.style.marginBottom = "0.2em";
+      glowInText.append(span,p);
+
+    } else {
+
+      glowInText.append(span);
+
+    }
+  });
+
+}
