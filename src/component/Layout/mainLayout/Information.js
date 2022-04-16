59f9b8f9f641fff12e1eb1eaa03b924341b4b55e
storm <stooormix@gmail.com>
2022-04-16 23:33:05 +0430

    replace img information with new picture chenge brightness img information set information_picture in information layout and remove <br/> in text

--- src/component/Layout/mainLayout/Information.js
+++ src/component/Layout/mainLayout/Information.js
@@ -12,10 +12,10 @@ export default function Information() {
       {/* <div id='information' className='nav-space'> */}
       {/* </div> */}
       <section className='info_layout_css'>
-        <img id='img_info' src='./IMG/information/final/IMG_20220210_180308.jpg' />
+        <img id='img_info' src='./IMG/information/final/information_picture.jpeg' />
         <div className='text_img_container'>
           <h3 className=''>اطلاعات کلی درباره من</h3><hr />
-          <p className='glowIn_text_info'>علی پارسامنش 20 ساله از مشهد. <br/> برنامه نویس ویندوز و وب. <br/> برنامه نویسی رو دوساله که شروع کردم ، با ویندوز استارت زدم و بعد از آموزش کوچ کردم به طراحی وبسایت.<br/>علاقمند به تکنولوژی های جدید و نو ظهور و همچنین خیلی قدیمی.<br/></p>
+          <p className='glowIn_text_info'>علی پارسامنش 20 ساله از مشهد.  برنامه نویس ویندوز و وب.  برنامه نویسی رو دوساله که شروع کردم ، با ویندوز استارت زدم و بعد از آموزش کوچ کردم به طراحی وبسایت.علاقمند به تکنولوژی های جدید و نو ظهور و همچنین خیلی قدیمی.</p>
         </div>
       </section>
     </Fragment>
